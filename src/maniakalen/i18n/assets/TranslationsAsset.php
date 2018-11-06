e03def567e9ae396546455d471b4a5db1d0e6312
Peter Georgiev <pesho10@yahoo.com>
2018-11-06 09:52:01 +0100

    Adding styles asset

---
+++ src/maniakalen/i18n/assets/TranslationsAsset.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peter.georgiev
+ * Date: 06/11/2018
+ * Time: 9:46
+ */
+
+namespace maniakalen\i18n\assets;
+
+
+class TranslationsAsset  extends AssetBundle
+{
+    public $sourcePath = '@translations/resources';
+
+    public $css = [
+        'css/styles.css',
+    ];
+}
