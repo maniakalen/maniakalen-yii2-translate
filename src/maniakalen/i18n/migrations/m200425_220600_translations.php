650166f6a8072d70aa464cf8208c6cbf4f7a8025
peter.georgiev <c00p3r4o+>
2020-04-26 14:29:14 +0200

    Fixing module class

--- src/maniakalen/i18n/migrations/m200425_220600_translations.php
+++ src/maniakalen/i18n/migrations/m200425_220600_translations.php
@@ -6,7 +6,7 @@ namespace maniakalen\i18n\migrations;
 
 class m200425_220600_translations extends \yii\db\Migration
 {
-    public function saveUp()
+    public function safeUp()
     {
         try {
             $this->batchInsert('source_message', ['category', 'message'], [
@@ -18,17 +18,21 @@ class m200425_220600_translations extends \yii\db\Migration
                 ['mi18n', 'Language'],
                 ['mi18n', 'Status'],
             ]);
+            return true;
         } catch (\Exception $e) {
             \Yii::error($e->getMessage(), 'mi18n');
         }
+        return false;
     }
 
-    public function saveDown()
+    public function safeDown()
     {
         try {
             $this->delete('source_message', ['category' => 'mi18n']);
+            return true;
         } catch (\Exception $e) {
             \Yii::error($e->getMessage(), 'mi18n');
         }
+        return false;
     }
 }
